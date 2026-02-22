3b3fdfadcbafddd9af7e2151e29926293be069f1
godmode-dev <gokronipa@icloud.com>
2026-02-22 19:53:37 +0000

    deploy to github

--- xpress-app/src/MainLayout/mainlayout.jsx
+++ xpress-app/src/MainLayout/mainlayout.jsx
@@ -5,7 +5,7 @@ import DashboardLayout from "../DashBoardLayout/dashboardLayout"; // New compone
 
 const MainLayout = () => {
     return (
-        <BrowserRouter basename="/admin/">
+        <BrowserRouter basename="/xpress-admin">
             <Routes>
                 {/* Public routes (no navbar) */}
                 <Route path="/login" element={<LoginPage />} />
