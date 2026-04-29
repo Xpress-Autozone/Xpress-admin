a7eb8a28c12c9e52eac9247700a1f9c8e97d18a9
GodwinOkronipa <gokronipa@icloud.com>
2026-04-29 11:27:20 +0000

    Deploying to gh-pages from @ Xpress-Autozone/Xpress-admin@c4b506b7291db8aa1cf9dc2943fba1cd963ec9fa 🚀

---
+++ assets/message-square-BN3LN4U3.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-DV_O3v5j.js";/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],s=a("external-link",e);/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z",key:"18887p"}]],n=a("message-square",t);export{s as E,n as M};
